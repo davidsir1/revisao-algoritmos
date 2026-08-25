1a4d793b2ea092f4074580b8dafda86f28437a71
Davi Lima <davidlima35566@gmail.com>
2026-08-25 12:47:55 -0300

    Resolução da questão 11 da lista

---
+++ Exercicio-11/main.c
@@ -0,0 +1,27 @@
+int main() {
+
+    /*
+     * a) Qual a diferença entre as estruturas pilha e fila?
+     * Fila é uma estrutura de dados linear em que o primeiro elemento a entrar é o primeiro a sair e pilha o último
+     * elemento inserido é o primeiro a ser retirado da lista.
+     *
+     * b) Quais as vantagens da fila? Quais suas desvantagens?
+     * Garante que os dados sejam processados na ordem de chegada, a inserção e a remoção é extremamente rápido. A
+     * desvantagem é não permitir acesso aleatório, pesquisar um valor exige percorrer item por item.
+     *
+     * c) Quais as vantagens da pilha? Quais suas desvantagens?
+     * As vantagens da pilha é as operações de inserção e remoção acontecem apenas no topo, os dados inseridos e
+     * removidos estão em uma ordem estrita. As desvantagens é não permitir reorganizar itens internamente ou processar
+     * prioridade.
+     *
+     * d) Quais suas aplicações?
+     * Pilhas são usadas em histórico de navegação, expressões matemáticas, recurso de desfazer. Filas são usadas
+     * em escalonador de processos, sistemas de mensageria e microsserviços.
+     *
+     * e) Quais as diferenças deles para lista?
+     * Lista tem mais liberdade de acesso em comparação com pilha e fila. Nós podemos inserir, ler e remover elementos
+     * em qualquer posição.
+     */
+
+    return 0;
+}
